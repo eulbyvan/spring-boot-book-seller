d87691438b3fe1f0b0e73e36cbab51f5516bc852
eulbyvan <work.issdq@gmail.com>
2022-12-07 20:27:10 +0700

    add User and Role classes

---
+++ src/main/java/com/eulbyvan/springbootbookseller/model/Role.java
@@ -0,0 +1,11 @@
+package com.eulbyvan.springbootbookseller.model;/**
+ * @author stu (https://www.eulbyvan.com/)
+ * @version 1.0
+ * @since 07/12/2022
+ */
+
+public enum Role {
+    USER, // default
+    ADMIN, // admin manager, CRUD
+    SYSTEM_MANAGER // internal operations
+}
