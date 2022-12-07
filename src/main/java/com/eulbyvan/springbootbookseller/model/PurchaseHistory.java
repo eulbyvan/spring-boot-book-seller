f27b4ca473cbd5f8b5e6350ea1312efb056e9681
eulbyvan <work.issdq@gmail.com>
2022-12-07 20:51:30 +0700

    Create PurchaseHistory.java

---
+++ src/main/java/com/eulbyvan/springbootbookseller/model/PurchaseHistory.java
@@ -0,0 +1,29 @@
+package com.eulbyvan.springbootbookseller.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author stu (https://www.eulbyvan.com/)
+ * @version 1.0
+ * @since 07/12/2022
+ */
+
+@Data
+@Entity
+@Table(name = "purchase_history")
+public class PurchaseHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+    @Column(name = "book_id", nullable = false)
+    private Long bookId;
+    @Column(name = "price", nullable = false)
+    private Double price;
+    @Column(name = "purchase_time", nullable = false)
+    private LocalDateTime purchaseTime;
+}
