3c99cf6fbe55655942c69e9a4a063c1f567114fa
eulbyvan <work.issdq@gmail.com>
2022-12-07 21:10:04 +0700

    Create IUserRepository.java

---
+++ src/main/java/com/eulbyvan/springbootbookseller/repository/IUserRepository.java
@@ -0,0 +1,23 @@
+package com.eulbyvan.springbootbookseller.repository;
+
+import com.eulbyvan.springbootbookseller.model.Role;
+import com.eulbyvan.springbootbookseller.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+/**
+ * @author stu (https://www.eulbyvan.com/)
+ * @version 1.0
+ * @since 07/12/2022
+ */
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    @Modifying
+    @Query("update User set role = :role where username = :usernam e")
+    void updateUserRole(@Param("username") String username, @Param("role") Role role);
+}
